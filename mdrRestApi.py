b4d8cc9f4c04704dfd1ac9fe0c4e9f8294b0e6f9
Dillibabu2357 <dillibabudommaraju@gmail.com>
2024-08-21 10:04:56 +0530

    commit message

--- mdrRestApi.py
+++ mdrRestApi.py
@@ -1,30 +1,116 @@
-__author__ = 'naras_mg'
+''' MDR Dashboard Service
+======================
 
-# libraries
-from flask import Flask, jsonify
-from flask_cors import cross_origin
+This service provides API endpoints to retrieve and display dashboard data for various applications. It also provides an API documentation endpoint using Swagger.
+
+Author: Your Name
+
+Dependencies:
+-------------
+- Flask: A micro web framework for Python.
+- Flask-RESTx: An extension for creating RESTful APIs with Swagger documentation.
+- PyYAML: A YAML parser and emitter for Python.
+- sqlDashboardCountsQuery: A custom module for querying SQL databases for dashboard counts.'''
+
+
+from flask import Flask
+from flask_restx import Api, Resource, fields
 import yaml
-from yaml import SafeLoader
 import sqlDashboardCountsQuery
 
-yamlFile = yaml.load(open("db_credentials.yaml"), SafeLoader)
-
 app = Flask(__name__)
+api = Api(app, version='1.0', title='MDR Dashboard', description='MDR Dashboard API')
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(jsonify({'error': 'Not found'}), 404)
-@app.route('/')
-@cross_origin()
-def hello():
-  return 'Hello from mdr dashboard service!'
-@app.route('/app/<string:prefix>')
-@cross_origin()
-def dashboard(prefix):
-  return jsonify(sqlDashboardCountsQuery.getDataQryResults(yamlFile[prefix]['db']))
-@app.route('/allcounts')
-@cross_origin()
-def dashboard_sqlQuery():
-    return jsonify(sqlDashboardCountsQuery.getDataQryResultsAll())
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+yamlFile = yaml.load(open("db_credentials.yaml"), yaml.SafeLoader)
+
+dashboard_model = api.model('Dashboard', {
+    'Manuscripts': fields.Integer(description='Count of Manuscripts'),
+    'Books': fields.Integer(description='Count of Books'),
+    'Articles': fields.Integer(description='Count of Articles'),
+    'Users': fields.Integer(description='Count of Users')
+})
+
+allcounts_model = api.model('AllCounts', {
+    'database_name': fields.Nested(dashboard_model, description='Counts of documents and users for a mdrDashboard database')
+})
+
+
+
+
+@api.route('/spec')
+class SwaggerSpec(Resource):
+    def get(self):
+        """
+        Swagger API Documentation Endpoint
+        ----------------------------------
+        Returns the Swagger API documentation for this service.
+
+        Returns:
+            JSON: Swagger documentation for the MDR Dashboard service.
+        """
+        return api.spec
+
+
+
+
+@api.route('/')
+class HelloWorld(Resource):
+    def get(self):
+        """
+        Root Endpoint
+        -------------
+        A simple endpoint to test if the service is running. It returns a welcome message.
+
+        Returns:
+            dict: A welcome message.
+        """
+        return {'message': 'Hello from MDR Dashboard service!'}
+
+    
+
+
+
+@api.route('/app/<string:prefix>')
+class Dashboard(Resource):
+    @api.doc(params={'prefix': 'The prefix that identifies the application.'})
+    @api.marshal_with(dashboard_model)
+    def get(self, prefix):
+        """
+        Dashboard Data for a Specific App
+        ---------------------------------
+        Returns dashboard data for a specific application based on the provided prefix.
+
+        Args:
+            prefix (str): The prefix that identifies the application.
+
+        Returns:
+            JSON: The dashboard data for the specified application.
+        """
+        return sqlDashboardCountsQuery.getDataQryResults(yamlFile[prefix]['db'])
+
+
+
+
+
+@api.route('/allcounts')
+class AllCounts(Resource):
+    @api.marshal_with(allcounts_model)
+    def get(self):
+        """
+        Aggregated Dashboard Data
+        -------------------------
+        Returns aggregated dashboard data from all databases.
+
+        Returns:
+            JSON: Aggregated dashboard data across all databases.
+        """
+        return sqlDashboardCountsQuery.getDataQryResultsAll()
+
+
+
+
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    run_simple("127.0.0.1", 5000, app)
+
--- sqlDashboardCountsQuery.py
+++ sqlDashboardCountsQuery.py
@@ -18,6 +18,7 @@ else:
     else:
         database = MySQLDatabase(db, host=database_url, port=3306, user=db_user, password=db_password)
         print('db url: %s'%database_url)
+
 def getDataQryResults(dbn):
     yamlFile = yaml.load(open("db_credentials.yaml"), SafeLoader)
     keys = {1:'Manuscripts', 2:'Books', 3:'Articles'}
@@ -43,6 +44,8 @@ def getDataQryResults(dbn):
     # res = cursor.fetchone()
     # print('Total: ', res[0])
     return dictDocs
+
+
 def getDataQryResultsAll():
     yamlFile = yaml.load(open("db_credentials.yaml"), SafeLoader)
     keys = {1:'Manuscripts', 2:'Books', 3:'Articles'}
@@ -75,7 +78,49 @@ def getDataQryResultsAll():
     # print('Docs\n%s'%dictDocs)
     return dictDocs
 
+
+
+# def getDataQryResultsAll():
+#     yamlFile = yaml.load(open("db_credentials.yaml"), SafeLoader)
+#     keys = {1:'Manuscripts', 2:'Books', 3:'Articles'}
+#     dictUsers = {}; dictDocs = {}
+#     jsonKeys = yamlFile['jsonKeys']; mdrDatabases = list(jsonKeys.keys())
+#     for dbn in mdrDatabases: dictDocs[jsonKeys[dbn]] = {'Manuscripts': 0, 'Books': 0, 'Articles': 0, 'Users': 0}
+#     dbn = mdrDatabases[0]
+#     txt = 'SELECT "{dbn:s}", DocumentType, COUNT(DocumentType) FROM {dbn:s}.omds_digital_manuscript WHERE isDeleted=0 GROUP BY DocumentType'
+#     qryDocs = txt.format(dbn=dbn)
+#     txt = 'SELECT "{dbn:s}", COUNT(*) FROM {dbn:s}.omds_employeemaster WHERE IsDeleted=0'
+#     qryUsers = txt.format(dbn=dbn)
+#     for dbn in mdrDatabases[1:]:
+#         txt = ' UNION SELECT "{dbn:s}", DocumentType, COUNT(DocumentType) FROM {dbn:s}.omds_digital_manuscript WHERE isDeleted=0 GROUP BY DocumentType'
+#         qryDocs += txt.format(dbn=dbn)
+#         txt = ' UNION SELECT "{dbn:s}", COUNT(*) FROM {dbn:s}.omds_employeemaster WHERE IsDeleted=0'
+#         qryUsers += txt.format(dbn=dbn)
+#         dictDocs[jsonKeys[dbn]] = {'Manuscripts': 0, 'Books': 0, 'Articles': 0, 'Users': 0}
+#     qryDocs += ';'; qryUsers += ';'
+#     # print('%s\n%s\n%18s %s'%(qryUsers, 'Users', 'Database', 'Total'))
+#     cursor = database.execute_sql(qryUsers)
+#     for row in cursor.fetchall():
+#         # print('%18s %4d'%(row[0], row[1]))
+#         dictUsers[row[0]] = row[1]
+#     # print('%s\n%s\n%18s %s %s'%(qryDocs, 'Documents', 'Database', 'Type', 'Total'))
+#     cursor = database.execute_sql(qryDocs)
+#     for row in cursor.fetchall():
+#         # print('%18s %4d %6d'%(row[0], row[1], row[2]))
+#         dictDocs[jsonKeys[row[0]]][keys[row[1]]] = row[2]
+#     for k, v in dictUsers.items(): dictDocs[jsonKeys[k]]['Users'] = v
+#     # print('Docs\n%s'%dictDocs)
+#     return dictDocs
+
+
+
+
+
+
 if __name__ == '__main__':
     yamlFile = yaml.load(open("db_credentials.yaml"), SafeLoader)
     mdrDatabases = yamlFile['jsonKeys'].keys()
     print('all: %s\n%s' % (getDataQryResultsAll(), [getDataQryResults(dbn) for dbn in mdrDatabases]))
+
+
+
